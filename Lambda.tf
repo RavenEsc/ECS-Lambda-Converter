ec64c8617d25493d6716813a6a654f25eedeb067
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2024-03-25 14:46:38 -0500

    Added default ECR Lambda module

---
+++ Lambda.tf
@@ -0,0 +1,11 @@
+module "lambda_function_container_image" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "Converter-Lambda-Script"
+  description   = "This is meant to convert files to other file types"
+
+  create_package = false
+
+  image_uri    = "132367819851.dkr.ecr.eu-west-1.amazonaws.com/complete-cow:1.0"
+  package_type = "Image"
+}
