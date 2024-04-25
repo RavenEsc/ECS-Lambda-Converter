a24a9e9896bf32c20ffaacd02189c807ad826e66
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2024-04-25 17:34:18 -0500

    added autoscaling capabities to ECS module

--- tf/ECS.tf
+++ tf/ECS.tf
@@ -1,7 +1,41 @@
 module "ecs" {
   source = "terraform-aws-modules/ecs/aws"
 
-  cluster_name = "ecs-integrated"
+  cluster_name = "ecs-ec2"
+
+  autoscaling_capacity_providers = {
+    one = {
+      auto_scaling_group_arn         = "arn:aws:autoscaling:${var.REGION}:${var.ACCOUNT_NUM}:autoScalingGroup:08419a61:autoScalingGroupName/ecs-ec2-one-20220603194933774300000011"
+      managed_termination_protection = "ENABLED"
+
+      managed_scaling = {
+        maximum_scaling_step_size = 5
+        minimum_scaling_step_size = 1
+        status                    = "ENABLED"
+        target_capacity           = 60
+      }
+
+      default_capacity_provider_strategy = {
+        weight = 60
+        base   = 20
+      }
+    }
+    two = {
+      auto_scaling_group_arn         = "arn:aws:autoscaling:${var.REGION}:${var.ACCOUNT_NUM}:autoScalingGroup:08419a61:autoScalingGroupName/ecs-ec2-two-20220603194933774300000022"
+      managed_termination_protection = "ENABLED"
+
+      managed_scaling = {
+        maximum_scaling_step_size = 15
+        minimum_scaling_step_size = 5
+        status                    = "ENABLED"
+        target_capacity           = 90
+      }
+
+      default_capacity_provider_strategy = {
+        weight = 40
+      }
+    }
+  }
 
   services = {
     ecs-nginx = {
@@ -20,6 +54,7 @@ module "ecs" {
               protocol      = "tcp"
             }
           ]
+          enable_cloudwatch_logging = false
         }
       }
 
@@ -42,7 +77,9 @@ module "ecs" {
           container_port   = 80
         }
       }
+
       subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+
       security_group_rules = {
         alb_ingress_3000 = {
           type                     = "ingress"
