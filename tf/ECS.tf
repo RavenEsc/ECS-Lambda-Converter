0b9767ebd2f26de8f4400751763f08bd6d612531
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2024-04-25 16:30:43 -0500

    Added a variable file and testing the ECS Module.

--- tf/ECS.tf
+++ tf/ECS.tf
@@ -25,28 +25,9 @@ module "ecs" {
 
       load_balancer = {
         service = {
-          target_group_arn = "arn:aws:elasticloadbalancing:eu-west-1:1234567890:targetgroup/bluegreentarget1/209a844cd01825a4"
+          target_group_arn = "arn:aws:elasticloadbalancing:${var.REGION}:${var.ACCOUNT_NUM}:targetgroup/bluegreentarget1/209a844cd01825a4"
           container_name   = "nginx"
           container_port   = 80
-        }
-      }
-
-      subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
-      security_group_rules = {
-        alb_ingress_80 = {
-          type                     = "ingress"
-          from_port                = 80
-          to_port                  = 80
-          protocol                 = "tcp"
-          description              = "Service port"
-          source_security_group_id = "sg-12345678"
-        }
-        egress_all = {
-          type        = "egress"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
         }
       }
     }
---
+++ tf/variables.tf
@@ -0,0 +1,10 @@
+variable "REGION" {
+    type = string
+    default = "us-east-1"
+    description = "The region that the code is deployed in"
+}
+
+variable "ACCOUNT_NUM" {
+    type = string
+    default = "464004139021"
+}
