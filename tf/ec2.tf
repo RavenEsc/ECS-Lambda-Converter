6190306904d60b80d776b1e29f100aceb8d6645d
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2024-05-07 22:24:41 -0500

    added my own role_policy

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_launch_template" "ecs_lt" {
   key_name = "ecskey1"
   vpc_security_group_ids = [aws_security_group.security_group.id]
   iam_instance_profile {
-    name = "ecsInstanceRole"
+    name = aws_iam_instance_profile.ecs_instance_profile.name
   }
 
   block_device_mappings {
@@ -33,6 +33,39 @@ resource "aws_launch_template" "ecs_lt" {
 
   user_data = filebase64("${path.module}/ecs.sh")
 }
+
+# Create the IAM Role for the ECS task
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-task-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Attach the necessary permissions to the IAM Role
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_role.name
+}
+
+# Create the IAM Instance Profile
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_task_role.name
+}
+
 
 resource "aws_autoscaling_group" "ecs_asg" {
   vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
